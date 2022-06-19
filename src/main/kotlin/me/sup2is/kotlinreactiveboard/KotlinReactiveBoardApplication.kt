f43db2b8e63e3869c853d4b5f979cf19435fb5f0
sup2is <dev.sup2is@gmail.com>
2022-06-19 12:58:42 +0900

    Initialize

---
+++ src/main/kotlin/me/sup2is/kotlinreactiveboard/KotlinReactiveBoardApplication.kt
@@ -0,0 +1,11 @@
+package me.sup2is.kotlinreactiveboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinReactiveBoardApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinReactiveBoardApplication>(*args)
+}
---
+++ src/test/kotlin/me/sup2is/kotlinreactiveboard/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.sup2is.kotlinreactiveboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
