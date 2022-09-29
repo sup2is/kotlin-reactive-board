c959be14482085592651afbdd377284e7e92fbb8
sup2is <dev.sup2is@gmail.com>
2022-09-29 23:34:08 +0900

    receiver: kafka receiver 테스트 구현중 ..

--- receiver/src/test/kotlin/me/sup2is/kotlinreactiveboard/receiver/receiver/TestProducerConfig.kt
+++ receiver/src/test/kotlin/me/sup2is/kotlinreactiveboard/receiver/receiver/TestProducerConfig.kt
@@ -1,32 +1,31 @@
 package me.sup2is.kotlinreactiveboard.receiver.receiver
 
 import me.sup2is.kotlinreactiveboard.receiver.config.KafkaProperties
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import reactor.kafka.receiver.KafkaReceiver
-import reactor.kafka.receiver.ReceiverOptions
+import reactor.kafka.sender.KafkaSender
+import reactor.kafka.sender.SenderOptions
 import java.util.*
 
 @TestConfiguration
 class TestProducerConfig {
 
     @Bean
-    fun producer(kafkaProperties: KafkaProperties): KafkaReceiver<String, String>? {
-        return KafkaReceiver.create(getProducerOptions(kafkaProperties))
+    fun producer(kafkaProperties: KafkaProperties): KafkaSender<String, String>? {
+        return KafkaSender.create(getProducerOptions(kafkaProperties))
     }
 
-    private fun getProducerOptions(kafkaProperties: KafkaProperties): ReceiverOptions<String, String> {
+    private fun getProducerOptions(kafkaProperties: KafkaProperties): SenderOptions<String, String> {
         val properties = Properties()
 
-        properties[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
-        properties[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        properties[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        properties[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
+        properties[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        properties[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
 
-        return ReceiverOptions.create<String?, String?>(properties)
-            .subscription(listOf(kafkaProperties.topic))
+        return SenderOptions.create(properties)
     }
 
 }
